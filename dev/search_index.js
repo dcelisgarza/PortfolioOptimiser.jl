db52768966cc033e37a9c951d8b2cb70e8c8524e
Documenter.jl <documenter@juliadocs.github.io>
2022-05-16 00:23:03 +0000

    build based on fc72cf3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PortfolioOptimiser","category":"page"},{"location":"#PortfolioOptimiser","page":"Home","title":"PortfolioOptimiser","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PortfolioOptimiser.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PortfolioOptimiser]","category":"page"}]
+}
