13e6b36c6098cc8cf5d3357ae4b9bdbfe9d40a05
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2022-06-13 19:47:33 +0100

    All coverage.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,26 +4,26 @@ using SafeTestsets
     include("ExpectedReturnsTests.jl")
 end
 
-# @safetestset "Risk models" begin
-#     include("RiskModelsTests.jl")
-# end
+@safetestset "Risk models" begin
+    include("RiskModelsTests.jl")
+end
 
-# @safetestset "Black Litterman" begin
-#     include("BlackLittermanTests.jl")
-# end
+@safetestset "Black Litterman" begin
+    include("BlackLittermanTests.jl")
+end
 
-# @safetestset "Efficient Frontier" begin
-#     include("EfficientFrontierTests.jl")
-# end
+@safetestset "Efficient Frontier" begin
+    include("EfficientFrontierTests.jl")
+end
 
-# @safetestset "Hierarchical Risk Parity" begin
-#     include("HRPOptTests.jl")
-# end
+@safetestset "Hierarchical Risk Parity" begin
+    include("HRPOptTests.jl")
+end
 
-# @safetestset "Critical Line" begin
-#     include("CriticalLineTests.jl")
-# end
+@safetestset "Critical Line" begin
+    include("CriticalLineTests.jl")
+end
 
-# @safetestset "Custom optimiser" begin
-#     include("CustomOptimiserTests.jl")
-# end
+@safetestset "Custom optimiser" begin
+    include("CustomOptimiserTests.jl")
+end
