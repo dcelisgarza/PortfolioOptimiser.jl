28f3044873b54bac604405e33cd2424ad6fc1cf1
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2023-07-10 19:44:26 +0100

    Block vectorisation.

--- src/Portfolio/Aux_functions.jl
+++ src/Portfolio/Aux_functions.jl
@@ -1,1 +1,24 @@
-function block_vec_pq() end
+function block_vec_pq(A, p, q)
+    mp, nq = size(A)
+
+    !(mod(mp, p) == 0 && mod(nq, p) == 0) && (throw(
+        DimensionMismatch(
+            "dimensions A, $(size(A)), must be integer multiples of (p, q) = ($p, $q)",
+        ),
+    ))
+
+    m = Int(mp / p)
+    n = Int(nq / q)
+
+    A_vec = Matrix{eltype(A)}(undef, m * n, p * q)
+    for j in 0:(n - 1)
+        Aj = Matrix{eltype(A)}(undef, m, p * q)
+        for i in 0:(m - 1)
+            Aij = vec(A[(1 + (i * p)):((i + 1) * p), (1 + (j * q)):((j + 1) * q)])
+            Aj[i + 1, :] .= Aij
+        end
+        A_vec[(1 + (j * m)):((j + 1) * m), :] .= Aj
+    end
+
+    return A_vec
+end
