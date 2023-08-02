d53f1463ff1eb7773cb194b2b1aa64576b6d9f8f
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2023-08-02 22:26:59 +0100

    More hrp

--- src/Portfolio/Risk_measures.jl
+++ src/Portfolio/Risk_measures.jl
@@ -1,1 +1,20 @@
-function sharpe_risk(w, cov, returns, rm, rf, alpha, a_sim, beta, b_sim, kappa, solver) end
+function sharpe_risk(
+    w,
+    cov,
+    returns,
+    rm = :mv,
+    rf = 0.0,
+    alpha = 0.05,
+    a_sim = 100,
+    beta = Inf,
+    b_sim = -1,
+    kappa = 0.3,
+    solver = nothing,
+)
+    if rm == :mv
+        risk = dot(w, cov, w)
+    elseif rm == :msd
+        risk = sqrt(dot(w, cov, w))
+    elseif rm == :mad
+    end
+end
