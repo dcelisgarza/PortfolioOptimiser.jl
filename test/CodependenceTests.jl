b252f08175814a559fe4f277eef53ad76dd8e765
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2023-08-24 23:05:21 +0100

    Try changing python env

--- test/CodependenceTests.jl
+++ test/CodependenceTests.jl
@@ -1,6 +1,7 @@
 using Test, PortfolioOptimiser, CSV, TimeSeries, DataFrames, Conda
 
-Conda.add("astropy.stats")
+ENV["PYTHON"] = ""
+Conda.add("astropy")
 A = TimeArray(CSV.File("./assets/stock_prices.csv"), timestamp = :date)
 Y = percentchange(A)
 returns = dropmissing!(DataFrame(Y))
