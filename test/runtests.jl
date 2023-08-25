2de98d3404495e9ff774e6d771433eb23f1fded6
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2023-08-25 21:13:49 +0100

    Forgot i'd commented out the tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,24 @@
 using SafeTestsets
 
-# @safetestset "Risk functions" begin
-#     include("RiskMeasureTests.jl")
-# end
+@safetestset "Risk functions" begin
+    include("RiskMeasureTests.jl")
+end
 
-# @safetestset "Codependence and distance functions" begin
-#     include("CodepDistTests.jl")
-# end
+@safetestset "Codependence and distance functions" begin
+    include("CodepDistTests.jl")
+end
 
-# @safetestset "Clustering" begin
-#     include("ClusteringTests.jl")
-# end
+@safetestset "Clustering" begin
+    include("ClusteringTests.jl")
+end
 
 @safetestset "Hierarchical optimisations" begin
     include("HierarchicalOptimisationTests.jl")
 end
 
-# @safetestset "Traditional optimisations" begin
-#     include("TraditionalOptimisationTests.jl")
-# end
+@safetestset "Traditional optimisations" begin
+    include("TraditionalOptimisationTests.jl")
+end
 
 # @safetestset "Expected returns" begin
 #     include("ExpectedReturnsTests.jl")
