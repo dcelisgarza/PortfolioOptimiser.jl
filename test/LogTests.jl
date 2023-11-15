f05d599fb6c39ceec1ea24154809e63c76932726
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2023-11-15 17:57:31 +0000

    Update LogTests.jl

--- test/LogTests.jl
+++ test/LogTests.jl
@@ -1,4 +1,4 @@
-using Logging, JuMP, PortfolioOptimiser, CSV, DataFrames, Dates
+using Logging, JuMP, PortfolioOptimiser, CSV, DataFrames, Dates, TimeSeries, ECOS, Clarabel, COSMO
 
 A = TimeArray(CSV.File("./assets/stock_prices.csv"), timestamp = :date)
 Y = percentchange(A)
