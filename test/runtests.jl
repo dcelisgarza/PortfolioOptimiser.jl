0e5da9acbb9024ef27e376a78b905a1086b31af6
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-04-25 18:28:38 +0100

    I want to know stats v2 coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,91 +4,91 @@ using SafeTestsets
     include("StatsTest_v2.jl")
 end
 
-@safetestset "Entropic and Relativistic RM tests" begin
-    include("EntrRelRMs.jl")
-end
+# @safetestset "Entropic and Relativistic RM tests" begin
+#     include("EntrRelRMs.jl")
+# end
 
-@safetestset "Portfolio Classic Traditional Optimisation" begin
-    include("PortfolioClassicTradTests.jl")
-end
+# @safetestset "Portfolio Classic Traditional Optimisation" begin
+#     include("PortfolioClassicTradTests.jl")
+# end
 
-@safetestset "HCPortfolio Optimisation" begin
-    include("HCPortfolioOptimisationTests.jl")
-end
+# @safetestset "HCPortfolio Optimisation" begin
+#     include("HCPortfolioOptimisationTests.jl")
+# end
 
-@safetestset "Asset Statistics" begin
-    include("AssetStatisticsTests2.jl")
-    include("AssetStatisticsTests1.jl")
-end
+# @safetestset "Asset Statistics" begin
+#     include("AssetStatisticsTests2.jl")
+#     include("AssetStatisticsTests1.jl")
+# end
 
-@safetestset "Misc" begin
-    include("MiscTests.jl")
-end
+# @safetestset "Misc" begin
+#     include("MiscTests.jl")
+# end
 
-@safetestset "Portfolio Traditional Optimisation Constraints" begin
-    include("PortfolioTradConstraintTests.jl")
-end
+# @safetestset "Portfolio Traditional Optimisation Constraints" begin
+#     include("PortfolioTradConstraintTests.jl")
+# end
 
-@safetestset "Constraint Functions" begin
-    include("ConstraintTests.jl")
-end
+# @safetestset "Constraint Functions" begin
+#     include("ConstraintTests.jl")
+# end
 
-@safetestset "Plotting" begin
-    include("PlottingTests.jl")
-end
+# @safetestset "Plotting" begin
+#     include("PlottingTests.jl")
+# end
 
-@safetestset "Efficient Frontier" begin
-    include("PortfolioEfficientFrontierTests.jl")
-end
+# @safetestset "Efficient Frontier" begin
+#     include("PortfolioEfficientFrontierTests.jl")
+# end
 
-@safetestset "Asset Allocation" begin
-    include("AssetAllocationTests.jl")
-end
+# @safetestset "Asset Allocation" begin
+#     include("AssetAllocationTests.jl")
+# end
 
-@safetestset "BL Statistics" begin
-    include("BLStatisticsTests.jl")
-end
+# @safetestset "BL Statistics" begin
+#     include("BLStatisticsTests.jl")
+# end
 
-@safetestset "Factor Statistics" begin
-    include("FactorStatisticsTests.jl")
-end
+# @safetestset "Factor Statistics" begin
+#     include("FactorStatisticsTests.jl")
+# end
 
-@safetestset "OWA Weights" begin
-    include("OWAWeightsTests.jl")
-end
+# @safetestset "OWA Weights" begin
+#     include("OWAWeightsTests.jl")
+# end
 
-@safetestset "Misc Statistics" begin
-    include("MiscStatisticsTests.jl")
-end
+# @safetestset "Misc Statistics" begin
+#     include("MiscStatisticsTests.jl")
+# end
 
-@safetestset "DBHT Clustering" begin
-    include("DBHTClusteringTests.jl")
-end
+# @safetestset "DBHT Clustering" begin
+#     include("DBHTClusteringTests.jl")
+# end
 
-@safetestset "Portfolio Traditional Class Picking" begin
-    include("PortfolioTradClassPickingTests.jl")
-end
+# @safetestset "Portfolio Traditional Class Picking" begin
+#     include("PortfolioTradClassPickingTests.jl")
+# end
 
-@safetestset "Portfolio Classic RP OWA Optimisation" begin
-    include("PortfolioClassicRPOWATests.jl")
-end
+# @safetestset "Portfolio Classic RP OWA Optimisation" begin
+#     include("PortfolioClassicRPOWATests.jl")
+# end
 
-@safetestset "HCPortfolio OWA Optimisation" begin
-    include("HCPortfolioOWAOptimisationTests.jl")
-end
+# @safetestset "HCPortfolio OWA Optimisation" begin
+#     include("HCPortfolioOWAOptimisationTests.jl")
+# end
 
-@safetestset "Portfolio Classic Traditional OWA Optimisation" begin
-    include("PortfolioClassicTradOWATests.jl")
-end
+# @safetestset "Portfolio Classic Traditional OWA Optimisation" begin
+#     include("PortfolioClassicTradOWATests.jl")
+# end
 
-@safetestset "Portfolio Classic WC Optimisation" begin
-    include("PortfolioClassicWCTests.jl")
-end
+# @safetestset "Portfolio Classic WC Optimisation" begin
+#     include("PortfolioClassicWCTests.jl")
+# end
 
-@safetestset "WC Statistics" begin
-    include("WCStatisticsTests.jl")
-end
+# @safetestset "WC Statistics" begin
+#     include("WCStatisticsTests.jl")
+# end
 
-@safetestset "Portfolio Classic RP Optimisation" begin
-    include("PortfolioClassicRPTests.jl")
-end
+# @safetestset "Portfolio Classic RP Optimisation" begin
+#     include("PortfolioClassicRPTests.jl")
+# end
