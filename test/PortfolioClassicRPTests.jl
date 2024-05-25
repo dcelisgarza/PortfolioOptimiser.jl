b026b58fcb928e9ba1e688dd4b1cfc2b5903d0cf
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-05-25 17:36:30 +0100

    Wrong path

--- test/PortfolioClassicRPTests.jl
+++ test/PortfolioClassicRPTests.jl
@@ -1,7 +1,7 @@
 using CSV, Clarabel, HiGHS, LinearAlgebra, OrderedCollections, PortfolioOptimiser,
       Statistics, Test, TimeSeries
 
-prices = TimeArray(CSV.File("./test/assets/stock_prices.csv"); timestamp = :date)
+prices = TimeArray(CSV.File("./assets/stock_prices.csv"); timestamp = :date)
 
 rf = 1.0329^(1 / 252) - 1
 l = 2.0
