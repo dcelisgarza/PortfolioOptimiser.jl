1dd7a30606edfde5c401e06dc933c1051252df3c
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-06-13 22:36:19 +0100

    Test tolerance

--- test/RiskMeasureTests_v2.jl
+++ test/RiskMeasureTests_v2.jl
@@ -30,6 +30,6 @@ prices = TimeArray(CSV.File("./assets/stock_prices.csv"); timestamp = :date)
            EDaR_r2(), RDaR_r2()]
 
     for (risk, rm) ∈ zip(risks, rms)
-        @test isapprox(risk, calc_risk(portfolio; type = :Trad, rm = rm))
+        @test isapprox(risk, calc_risk(portfolio; type = :Trad, rm = rm), rtol = 1e-7)
     end
 end
