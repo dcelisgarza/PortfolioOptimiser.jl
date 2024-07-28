9ee5cc0289c4915053800dd7c69ef5235bb44e37
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-07-29 00:07:55 +0100

    Fix broken windows tests

--- test/HCPortfolioOptimisationTests.jl
+++ test/HCPortfolioOptimisationTests.jl
@@ -3360,7 +3360,7 @@ end
     @test isapprox(w1.weights, w1t)
     @test isapprox(w2.weights, w2t)
     @test isapprox(w3.weights, w3t, rtol = 1.0e-8)
-    @test isapprox(w4.weights, w4t, rtol = 5.0e-8)
+    @test isapprox(w4.weights, w4t, rtol = 5.0e-6)
     @test isapprox(w5.weights, w5t, rtol = 1.0e-6)
     @test isapprox(w6.weights, w6t, rtol = 1.0e-9)
     @test isapprox(w7.weights, w7t, rtol = 0.0005)
@@ -3499,13 +3499,13 @@ end
     @test isapprox(w2.weights, w2t)
     @test isapprox(w3.weights, w3t)
     @test isapprox(w4.weights, w4t)
-    @test isapprox(w5.weights, w5t, rtol = 5.0e-8)
+    @test isapprox(w5.weights, w5t, rtol = 1.0e-7)
     @test isapprox(w6.weights, w6t)
-    @test isapprox(w7.weights, w7t, rtol = 0.0001)
+    @test isapprox(w7.weights, w7t, rtol = 0.0005)
     @test isapprox(w8.weights, w8t)
-    @test isapprox(w9.weights, w9t, rtol = 5.0e-8)
-    @test isapprox(w10.weights, w10t, rtol = 0.0001)
-    @test isapprox(w11.weights, w11t, rtol = 5.0e-8)
+    @test isapprox(w9.weights, w9t, rtol = 1.0e-7)
+    @test isapprox(w10.weights, w10t, rtol = 0.0005)
+    @test isapprox(w11.weights, w11t, rtol = 1.0e-7)
 end
 
 @testset "$(:HRP), $(:HERC), $(:NCO), Full $(:SKurt)" begin
