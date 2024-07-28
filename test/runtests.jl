9a55b5471fb6fda9f9b3492a8433e0cbcc09f787
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-07-28 21:02:21 +0100

    Fix more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,50 +4,6 @@ using SafeTestsets
     include("EntrRelRMs.jl")
 end
 
-@safetestset "Constraint Functions" begin
-    include("ConstraintTests.jl")
-end
-
-@safetestset "Plotting" begin
-    include("PlottingTests.jl")
-end
-
-@safetestset "Efficient Frontier" begin
-    include("PortfolioEfficientFrontierTests.jl")
-end
-
-@safetestset "Asset Allocation" begin
-    include("AssetAllocationTests.jl")
-end
-
-@safetestset "BL Statistics" begin
-    include("BLStatisticsTests.jl")
-end
-
-@safetestset "Factor Statistics" begin
-    include("FactorStatisticsTests.jl")
-end
-
-@safetestset "OWA Weights" begin
-    include("OWAWeightsTests.jl")
-end
-
-@safetestset "Misc Statistics" begin
-    include("MiscStatisticsTests.jl")
-end
-
-@safetestset "DBHT Clustering" begin
-    include("DBHTClusteringTests.jl")
-end
-
-@safetestset "Portfolio Traditional Class Picking" begin
-    include("PortfolioTradClassPickingTests.jl")
-end
-
-@safetestset "Portfolio Classic RP OWA Optimisation" begin
-    include("PortfolioClassicRPOWATests.jl")
-end
-
 @safetestset "HCPortfolio OWA Optimisation" begin
     include("HCPortfolioOWAOptimisationTests.jl")
 end
@@ -120,4 +76,48 @@ end
 
 @safetestset "Portfolio Traditional Optimisation Constraints" begin
     include("PortfolioTradConstraintTests.jl")
+end
+
+@safetestset "Constraint Functions" begin
+    include("ConstraintTests.jl")
+end
+
+@safetestset "Plotting" begin
+    include("PlottingTests.jl")
+end
+
+@safetestset "Efficient Frontier" begin
+    include("PortfolioEfficientFrontierTests.jl")
+end
+
+@safetestset "Asset Allocation" begin
+    include("AssetAllocationTests.jl")
+end
+
+@safetestset "BL Statistics" begin
+    include("BLStatisticsTests.jl")
+end
+
+@safetestset "Factor Statistics" begin
+    include("FactorStatisticsTests.jl")
+end
+
+@safetestset "OWA Weights" begin
+    include("OWAWeightsTests.jl")
+end
+
+@safetestset "Misc Statistics" begin
+    include("MiscStatisticsTests.jl")
+end
+
+@safetestset "DBHT Clustering" begin
+    include("DBHTClusteringTests.jl")
+end
+
+@safetestset "Portfolio Traditional Class Picking" begin
+    include("PortfolioTradClassPickingTests.jl")
+end
+
+@safetestset "Portfolio Classic RP OWA Optimisation" begin
+    include("PortfolioClassicRPOWATests.jl")
 end
