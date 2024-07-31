5304c3e358f984750aab0bd95c2e13483e951677
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-08-01 00:01:38 +0100

    fix path

--- test/TradOptimisationTests_v2.jl
+++ test/TradOptimisationTests_v2.jl
@@ -1,7 +1,7 @@
 using CSV, TimeSeries, DataFrames, StatsBase, Statistics, LinearAlgebra, Test, Clarabel,
       PortfolioOptimiser
 
-prices = TimeArray(CSV.File("./test/assets/stock_prices.csv"); timestamp = :date)
+prices = TimeArray(CSV.File("./assets/stock_prices.csv"); timestamp = :date)
 rf = 1.0329^(1 / 252) - 1
 l = 2.0
 
