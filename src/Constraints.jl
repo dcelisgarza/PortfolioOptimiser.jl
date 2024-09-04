c7922c46890c89ea7bb3fee689a437a6c78d0b36
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-09-04 21:42:09 +0100

    Restructuring

--- src/Constraints.jl
+++ src/Constraints.jl
@@ -736,5 +736,134 @@ function turnover_constraints(constraints::DataFrame, asset_sets::DataFrame)
     return turnover
 end
 
+function calc_centrality(method::BetweennessCentrality, G)
+    return Graphs.betweenness_centrality(G, method.args...; method.kwargs...)
+end
+function calc_centrality(method::ClosenessCentrality, G)
+    return Graphs.closeness_centrality(G, method.args...; method.kwargs...)
+end
+function calc_centrality(method::DegreeCentrality, G)
+    return Graphs._degree_centrality(G, method.type; method.kwargs...)
+end
+function calc_centrality(::EigenvectorCentrality, G)
+    return Graphs.eigenvector_centrality(G)
+end
+function calc_centrality(method::KatzCentrality, G)
+    return Graphs.katz_centrality(G, method.alpha)
+end
+function calc_centrality(method::Pagerank, G)
+    return Graphs.pagerank(G, method.alpha, method.n, method.epsilon)
+end
+function calc_centrality(::RadialityCentrality, G)
+    return Graphs.radiality_centrality(G)
+end
+function calc_centrality(method::StressCentrality, G)
+    return Graphs.stress_centrality(G, method.args...; method.kwargs...)
+end
+function clac_mst(method::KruskalTree, G)
+    return Graphs.kruskal_mst(G, method.args...; method.kwargs...)
+end
+function clac_mst(method::BoruvkaTree, G)
+    return Graphs.boruvka_mst(G, method.args...; method.kwargs...)
+end
+function clac_mst(method::PrimTree, G)
+    return Graphs.prim_mst(G, method.args...; method.kwargs...)
+end
+function _calc_adjacency(nt::TMFG, rho::AbstractMatrix, delta::AbstractMatrix)
+    S = dbht_similarity(nt.similarity, rho, delta)
+    Rpm = PMFG_T2s(S)[1]
+    return adjacency_matrix(SimpleGraph(Rpm))
+end
+function _calc_adjacency(nt::MST, ::Any, delta::AbstractMatrix)
+    G = SimpleWeightedGraph(delta)
+    tree = clac_mst(nt.tree, G)
+    return adjacency_matrix(SimpleGraph(G[tree]))
+end
+function _calc_adjacency(nt::NetworkType, X::AbstractMatrix,
+                         cor_type::PortfolioOptimiserCovCor, dist_type::DistanceMethod)
+    S = cor(cor_type, X)
+    dist_type = _get_default_dist(dist_type, cor_type)
+    D = dist(dist_type, S, X)
+    return _calc_adjacency(nt, S, D)
+end
+
+function connection_matrix(X::AbstractMatrix;
+                           cor_type::PortfolioOptimiserCovCor = PortCovCor(),
+                           dist_type::DistanceMethod = DistanceCanonical(),
+                           network_type::NetworkType = MST())
+    A = _calc_adjacency(network_type, X, cor_type, dist_type)
+
+    A_p = similar(Matrix(A))
+    fill!(A_p, zero(eltype(A_p)))
+    for i ∈ 0:(network_type.steps)
+        A_p .+= A^i
+    end
+
+    A_p .= clamp!(A_p, 0, 1) - I
+
+    return A_p
+end
+
+function centrality_vector(X::AbstractMatrix;
+                           cor_type::PortfolioOptimiserCovCor = PortCovCor(),
+                           dist_type::DistanceMethod = DistanceCanonical(),
+                           network_type::NetworkType = MST())
+    Adj = connection_matrix(X; cor_type = cor_type, dist_type = dist_type,
+                            network_type = network_type)
+    G = SimpleGraph(Adj)
+
+    return calc_centrality(network_type.centrality, G)
+end
+
+function cluster_matrix(X::AbstractMatrix;
+                        cor_type::PortfolioOptimiserCovCor = PortCovCor(),
+                        dist_type::DistanceMethod = DistanceCanonical(),
+                        hclust_alg::HClustAlg = HAC(), hclust_opt::HCOpt = HCOpt())
+    clusters = cluster_assets(X; cor_type = cor_type, dist_type = dist_type,
+                              hclust_alg = hclust_alg, hclust_opt = hclust_opt)[1]
+
+    N = size(X, 2)
+    A_c = Vector{Int}(undef, 0)
+    for i ∈ unique(clusters)
+        idx = clusters .== i
+        tmp = zeros(Int, N)
+        tmp[idx] .= 1
+        append!(A_c, tmp)
+    end
+
+    A_c = reshape(A_c, N, :)
+    A_c = A_c * transpose(A_c) - I
+
+    return A_c
+end
+
+function _con_rel(A::AbstractMatrix, w::AbstractVector)
+    ovec = range(; start = 1, stop = 1, length = size(A, 1))
+    aw = abs.(w * transpose(w))
+    C_a = transpose(ovec) * (A .* aw) * ovec
+    C_a /= transpose(ovec) * aw * ovec
+    return C_a
+end
+function connected_assets(returns::AbstractMatrix, w::AbstractVector;
+                          cor_type::PortfolioOptimiserCovCor = PortCovCor(),
+                          dist_type::DistanceMethod = DistanceCanonical(),
+                          network_type::NetworkType = MST())
+    A_c = connection_matrix(returns; cor_type = cor_type, dist_type = dist_type,
+                            network_type = network_type)
+    C_a = _con_rel(A_c, w)
+    return C_a
+end
+
+function related_assets(returns::AbstractMatrix, w::AbstractVector;
+                        cor_type::PortfolioOptimiserCovCor = PortCovCor(),
+                        dist_type::DistanceMethod = DistanceCanonical(),
+                        hclust_alg::HClustAlg = HAC(), hclust_opt::HCOpt = HCOpt())
+    A_c = cluster_matrix(returns; cor_type = cor_type, dist_type = dist_type,
+                         hclust_alg = hclust_alg, hclust_opt = hclust_opt)
+    R_a = _con_rel(A_c, w)
+    return R_a
+end
+
 export asset_constraints, factor_constraints, asset_views, factor_views, hrp_constraints,
-       rp_constraints, turnover_constraints
+       rp_constraints, turnover_constraints, calc_centrality, connection_matrix,
+       centrality_vector, cluster_matrix, connected_assets, related_assets
--- src/RiskMeasures.jl
+++ src/RiskMeasures.jl
@@ -22,6 +22,30 @@ function get_first_rm(rm::Union{AbstractVector, <:TradRiskMeasure})
     end
 end
 
+function set_rm_properties(rm, solvers, sigma)
+    solver_flag = false
+    sigma_flag = false
+    if hasproperty(rm, :solvers) && (isnothing(rm.solvers) || isempty(rm.solvers))
+        rm.solvers = solvers
+        solver_flag = true
+    end
+    if hasproperty(rm, :sigma) && (isnothing(rm.sigma) || isempty(rm.sigma))
+        rm.sigma = sigma
+        sigma_flag = true
+    end
+    return solver_flag, sigma_flag
+end
+
+function unset_set_rm_properties(rm, solver_flag, sigma_flag)
+    if solver_flag
+        rm.solvers = nothing
+    end
+    if sigma_flag
+        rm.sigma = nothing
+    end
+    return nothing
+end
+
 """
 ```
 _Variance(w::AbstractVector, Σ::AbstractMatrix)
@@ -1410,100 +1434,6 @@ function sharpe_ratio(rm::RiskMeasure, w::AbstractVector;
     end
     risk = calc_risk(rm, w; X = X, V = V, SV = SV, delta = delta)
     return (ret - rf) / risk
-end
-
-for (op, name) ∈
-    zip((SD, Variance, MAD, SSD, FLPM, SLPM, WR, VaR, CVaR, EVaR, RLVaR, DaR, MDD, ADD,
-         CDaR, UCI, EDaR, RLDaR, DaR_r, MDD_r, ADD_r, CDaR_r, UCI_r, EDaR_r, RLDaR_r, Kurt,
-         SKurt, GMD, RG, CVaRRG, TG, TGRG, OWA, dVar, Skew, SSkew, Equal),
-        ("SD", "Variance", "MAD", "SSD", "FLPM", "SLPM", "WR", "VaR", "CVaR", "EVaR",
-         "RLVaR", "DaR", "MDD", "ADD", "CDaR", "UCI", "EDaR", "RLDaR", "DaR_r", "MDD_r",
-         "ADD_r", "CDaR_r", "UCI_r", "EDaR_r", "RLDaR_r", "Kurt", "SKurt", "GMD", "RG",
-         "CVaRRG", "TG", "TGRG", "OWA", "dVar", "Skew", "SSkew", "Equal"))
-    eval(quote
-             Base.iterate(S::$op, state = 1) = state > 1 ? nothing : (S, state + 1)
-             function Base.String(s::$op)
-                 return $name
-             end
-             function Base.Symbol(::$op)
-                 return Symbol($name)
-             end
-             function Base.length(::$op)
-                 return 1
-             end
-             function Base.getindex(S::$op, I::Integer...)
-                 return S
-             end
-         end)
-end
-
-function set_rm_properties(rm, solvers, sigma)
-    solver_flag = false
-    sigma_flag = false
-    if hasproperty(rm, :solvers) && (isnothing(rm.solvers) || isempty(rm.solvers))
-        rm.solvers = solvers
-        solver_flag = true
-    end
-    if hasproperty(rm, :sigma) && (isnothing(rm.sigma) || isempty(rm.sigma))
-        rm.sigma = sigma
-        sigma_flag = true
-    end
-    return solver_flag, sigma_flag
-end
-
-function unset_set_rm_properties(rm, solver_flag, sigma_flag)
-    if solver_flag
-        rm.solvers = nothing
-    end
-    if sigma_flag
-        rm.sigma = nothing
-    end
-    return nothing
-end
-
-function calc_risk(port::AbstractPortfolio; X::AbstractMatrix = port.returns,
-                   type::Symbol = isa(port, Portfolio) ? :Trad : :HRP,
-                   rm::RiskMeasure = SD())
-    solver_flag, sigma_flag = set_rm_properties(rm, port.solvers, port.cov)
-    risk = calc_risk(rm, port.optimal[type].weights; X = X, V = port.V, SV = port.SV)
-    unset_set_rm_properties(rm, solver_flag, sigma_flag)
-    return risk
-end
-
-function risk_contribution(port::AbstractPortfolio; X::AbstractMatrix = port.returns,
-                           type::Symbol = isa(port, Portfolio) ? :Trad : :HRP,
-                           rm::RiskMeasure = SD(), delta::Real = 1e-6,
-                           marginal::Bool = false)
-    solver_flag, sigma_flag = set_rm_properties(rm, port.solvers, port.cov)
-    risk = risk_contribution(rm, port.optimal[type].weights; X = X, V = port.V,
-                             SV = port.SV, delta = delta, marginal = marginal)
-    unset_set_rm_properties(rm, solver_flag, sigma_flag)
-    return risk
-end
-
-function factor_risk_contribution(port::AbstractPortfolio;
-                                  type::Symbol = isa(port, Portfolio) ? :Trad : :HRP,
-                                  rm::RiskMeasure = SD(), delta::Real = 1e-6)
-    solver_flag, sigma_flag = set_rm_properties(rm, port.solvers, port.cov)
-    risk = factor_risk_contribution(rm, port.optimal[type].weights; X = port.returns,
-                                    assets = port.assets, F = port.f_returns,
-                                    f_assets = port.f_assets, B = port.loadings,
-                                    loadings_opt = port.loadings_opt, V = port.V,
-                                    SV = port.SV, delta = delta)
-    unset_set_rm_properties(rm, solver_flag, sigma_flag)
-    return risk
-end
-
-function sharpe_ratio(port::AbstractPortfolio; X::AbstractMatrix = port.returns,
-                      mu::AbstractVector = port.mu,
-                      type::Symbol = isa(port, Portfolio) ? :Trad : :HRP,
-                      rm::RiskMeasure = SD(), delta::Real = 1e-6, rf::Real = 0.0,
-                      kelly::Bool = false)
-    solver_flag, sigma_flag = set_rm_properties(rm, port.solvers, port.cov)
-    risk = sharpe_ratio(rm, port.optimal[type].weights; mu = mu, X = X, V = port.V,
-                        SV = port.SV, delta = delta, rf = rf, kelly = kelly)
-    unset_set_rm_properties(rm, solver_flag, sigma_flag)
-    return risk
 end
 
 export get_rm_string, get_first_rm, ERM, RRM, calc_risk, risk_bounds, risk_contribution,
