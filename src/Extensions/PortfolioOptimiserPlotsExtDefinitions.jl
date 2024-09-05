c6ea0bab43d3686dac093ca6cf6eb617b891c2ed
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-09-05 04:45:28 +0100

    Remove parenthesis from prototype functions

--- src/Extensions/PortfolioOptimiserPlotsExtDefinitions.jl
+++ src/Extensions/PortfolioOptimiserPlotsExtDefinitions.jl
@@ -3,18 +3,18 @@
 plot_returns
 ```
 """
-function plot_returns() end
-function plot_bar() end
-function plot_risk_contribution() end
-function plot_frontier_area() end
-function plot_drawdown() end
-function plot_hist() end
-function plot_range() end
-function plot_frontier() end
-function plot_clusters() end
-function plot_dendrogram() end
-function plot_network() end
-function plot_cluster_network() end
+function plot_returns end
+function plot_bar end
+function plot_risk_contribution end
+function plot_frontier_area end
+function plot_drawdown end
+function plot_hist end
+function plot_range end
+function plot_frontier end
+function plot_clusters end
+function plot_dendrogram end
+function plot_network end
+function plot_cluster_network end
 
 export plot_returns, plot_bar, plot_risk_contribution, plot_frontier_area, plot_drawdown,
        plot_hist, plot_range, plot_frontier, plot_clusters, plot_dendrogram, plot_network,
