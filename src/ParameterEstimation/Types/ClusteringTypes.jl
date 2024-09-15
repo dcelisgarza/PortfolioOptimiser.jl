6c8d552b8a73582a8878ad301cb2d01d612e6bb6
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-09-15 23:42:58 +0100

    bump deps

--- src/ParameterEstimation/Types/ClusteringTypes.jl
+++ src/ParameterEstimation/Types/ClusteringTypes.jl
@@ -162,6 +162,7 @@ Defines the options for processing clustering results in an instance of [`Cluste
 # Parameters
 
   - `branchorder`: parameter for ordering a dendrogram's branches accepted by [`Clustering.jl`](https://github.com/JuliaStats/Clustering.jl).
+
   - `k_method`: method subtyping [`NumClusterMethod`](@ref) for computing the number of clusters.
   - `k`: directly provide the number of clusters, if `0` use `k_method` for computing the number of clusters.
   - `max_k`: maximum number of clusters.
