049964e92cc0c91a5ff16a756326e594a58ba835
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-10-16 13:05:10 +0100

    Ensure allocate behaves well with short and short budget.

--- src/Optimisation/Functions/AssetAllocation.jl
+++ src/Optimisation/Functions/AssetAllocation.jl
@@ -15,8 +15,11 @@ function allocate!(port::AbstractPortfolio,
                    method::AllocationMethod = LP(), latest_prices = port.latest_prices,
                    investment::Real = 1e6, string_names::Bool = false,
                    short = isa(port, Portfolio) ? port.short : false,
-                   short_u = isa(port, Portfolio) ? port.short_u : 0,
-                   long_u = isa(port, Portfolio) ? port.long_u : 1)
+                   short_u = if isa(port, Portfolio)
+                       min(port.short_u, port.short_budget)
+                   else
+                       0
+                   end, long_u = isa(port, Portfolio) ? port.long_u : 1)
     return _allocate!(method, port, type, latest_prices, investment, short, long_u, short_u,
                       string_names)
 end
