97565b8db9f4f4c4b35846d7f0f2e6a748ee37eb
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-11-12 19:34:32 +0000

    Fix type tests path

--- test/TypeTests.jl
+++ test/TypeTests.jl
@@ -1,8 +1,8 @@
 using CSV, TimeSeries, DataFrames, StatsBase, Statistics, LinearAlgebra, Test, Clarabel,
       PortfolioOptimiser, JuMP, Clustering, SparseArrays
 
-prices = TimeArray(CSV.File("./test/assets/stock_prices.csv"); timestamp = :date)
-factors = TimeArray(CSV.File("./test/assets/factor_prices.csv"); timestamp = :date)
+prices = TimeArray(CSV.File("./assets/stock_prices.csv"); timestamp = :date)
+factors = TimeArray(CSV.File("./assets/factor_prices.csv"); timestamp = :date)
 
 rf = 1.0329^(1 / 252) - 1
 l = 2.0
