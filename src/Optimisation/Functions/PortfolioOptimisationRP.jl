21265cb0bf5ed32f53cba0d823f060c29e8eedc6
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-11-24 18:15:31 +0000

    Remove extra constraint

--- src/Optimisation/Functions/PortfolioOptimisationRP.jl
+++ src/Optimisation/Functions/PortfolioOptimisationRP.jl
@@ -105,8 +105,6 @@ function rp_constraints(port::OmniPortfolio, ::Any, w_ini)
     @constraints(model, begin
                      dot(risk_budget, log_w) >= 1
                      [i = 1:N], [log_w[i], 1, w[i]] ∈ MOI.ExponentialCone()
-                     w .>= 0
-                     sum(w) == k
                  end)
     return nothing
 end
