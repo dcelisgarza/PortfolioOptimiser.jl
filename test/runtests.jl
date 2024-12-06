ce622fe26b1ac8a002f9e9ea9cfba9c6a37bfc36
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-12-06 12:43:52 +0000

    Relax test tolerances.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,45 +1,5 @@
 using SafeTestsets
 
-@safetestset "Portfolio classes" begin
-    include("PortfolioClassesTests.jl")
-end
-
-@safetestset "Factor Stats tests" begin
-    include("FactorStatsTest.jl")
-end
-
-@safetestset "WC Stats tests" begin
-    include("WCStatsTest.jl")
-end
-
-@safetestset "Clustering tests" begin
-    include("ClusteringTests.jl")
-end
-
-@safetestset "DBHT Clustering" begin
-    include("DBHTClusteringTests.jl")
-end
-
-@safetestset "Constraint Functions" begin
-    include("ConstraintTests.jl")
-end
-
-@safetestset "Network constraint tests" begin
-    include("NetworkConstraintTests.jl")
-end
-
-@safetestset "BL Stats tests" begin
-    include("BLStatsTests.jl")
-end
-
-@safetestset "Stats tests" begin
-    include("StatsTest.jl")
-end
-
-@safetestset "RRP optimisation" begin
-    include("RRPOptimisationTests.jl")
-end
-
 @safetestset "Trad optimisation" begin
     include("TradOptimisationTests.jl")
 end
@@ -96,6 +56,45 @@ end
     include("MiscStatisticsTests.jl")
 end
 
+@safetestset "Portfolio classes" begin
+    include("PortfolioClassesTests.jl")
+end
+
+@safetestset "Factor Stats tests" begin
+    include("FactorStatsTest.jl")
+end
+
+@safetestset "WC Stats tests" begin
+    include("WCStatsTest.jl")
+end
+
+@safetestset "Clustering tests" begin
+    include("ClusteringTests.jl")
+end
+
+@safetestset "DBHT Clustering" begin
+    include("DBHTClusteringTests.jl")
+end
+
+@safetestset "Constraint Functions" begin
+    include("ConstraintTests.jl")
+end
+
+@safetestset "Network constraint tests" begin
+    include("NetworkConstraintTests.jl")
+end
+
+@safetestset "BL Stats tests" begin
+    include("BLStatsTests.jl")
+end
+
+@safetestset "Stats tests" begin
+    include("StatsTest.jl")
+end
+
+@safetestset "RRP optimisation" begin
+    include("RRPOptimisationTests.jl")
+end
 #=
 
 @safetestset "Type tests" begin
