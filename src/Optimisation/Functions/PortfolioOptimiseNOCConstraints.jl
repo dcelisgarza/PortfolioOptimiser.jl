22403778e284cbd46e83f41b28eeef03526cf095
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-12-30 11:33:34 +0900

    Add custom constraint and objectives and return w3 from internal NOC optimisations.

--- src/Optimisation/Functions/PortfolioOptimiseNOCConstraints.jl
+++ src/Optimisation/Functions/PortfolioOptimiseNOCConstraints.jl
@@ -56,13 +56,13 @@ function _noc_risks(rm, port, returns, sigma, w1, w2, w3)
     return risk1, risk2, risk3
 end
 function noc_risk_ret(port::Portfolio, type)
-    (; bins, w_min, w_max, w_min_ini, w_max_ini, w_opt, rm, obj, kelly, class, w_ini, ohf, scalarisation) = type
+    (; bins, w_min, w_max, w_min_ini, w_max_ini, w_opt, rm, obj, kelly, class, w_ini, custom_constr, custom_obj, ohf, scalarisation) = type
 
     mu, sigma, returns = mu_sigma_returns_class(port, class)
     w1 = if isempty(w_min)
         _w_min = optimise!(port,
                            Trad(; rm = rm, obj = MinRisk(), kelly = kelly, class = class,
-                                w_ini = w_min_ini))
+                                w_ini = w_min_ini, custom_constr = custom_constr, custom_obj = custom_obj))
         !isempty(_w_min) ? _w_min.weights : Vector{eltype(returns)}(undef, 0)
     else
         w_min
@@ -71,7 +71,7 @@ function noc_risk_ret(port::Portfolio, type)
     w2 = if isempty(w_max)
         _w_max = optimise!(port,
                            Trad(; rm = rm, obj = MaxRet(), kelly = kelly, class = class,
-                                w_ini = w_max_ini))
+                                w_ini = w_max_ini, custom_constr = custom_constr, custom_obj = custom_obj))
         !isempty(_w_max) ? _w_max.weights : Vector{eltype(returns)}(undef, 0)
     else
         w_max
@@ -80,7 +80,8 @@ function noc_risk_ret(port::Portfolio, type)
     w3 = if isempty(w_opt)
         _w_opt = optimise!(port,
                            Trad(; rm = rm, obj = obj, kelly = kelly, class = class,
-                                w_ini = w_ini, ohf = ohf, scalarisation = scalarisation))
+                                w_ini = w_ini, custom_constr = custom_constr, custom_obj = custom_obj,
+                                ohf = ohf, scalarisation = scalarisation))
         !isempty(_w_opt) ? _w_opt.weights : Vector{eltype(returns)}(undef, 0)
     else
         w_opt
@@ -104,5 +105,5 @@ function noc_risk_ret(port::Portfolio, type)
     ret3 -= d_ret
     risk3 += d_risk
 
-    return risk3, ret3
+    return w3, risk3, ret3
 end
