21ded456a5748849ef091e6f62ff35dcc8a0fc2d
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2024-12-30 21:17:48 +0900

    NOC scalarisation.

--- src/Optimisation/Functions/PortfolioOptimiseNOCConstraints.jl
+++ src/Optimisation/Functions/PortfolioOptimiseNOCConstraints.jl
@@ -26,7 +26,7 @@ function noc_constraints(port::Portfolio, risk0, ret0)
                  end)
     return nothing
 end
-function _noc_risks(rm, port, returns, sigma, w1, w2, w3)
+function _noc_risks(::ScalarSum, rm, port, returns, sigma, w1, w2, w3)
     rm = reduce(vcat, rm)
     risk1 = zero(eltype(returns))
     risk2 = zero(eltype(returns))
@@ -43,6 +43,27 @@ function _noc_risks(rm, port, returns, sigma, w1, w2, w3)
     end
     return risk1, risk2, risk3
 end
+function _noc_risks(scalarisation::ScalarLogSumExp, rm, port, returns, sigma, w1, w2, w3)
+    rm = reduce(vcat, rm)
+    risk1 = zero(eltype(returns))
+    risk2 = zero(eltype(returns))
+    risk3 = zero(eltype(returns))
+    gamma = scalarisation.gamma
+    for r ∈ rm
+        scale = r.settings.scale * gamma
+        solver_flag, sigma_flag, skew_flag, sskew_flag = set_rm_properties!(r, port.solvers,
+                                                                            sigma, port.V,
+                                                                            port.SV)
+        risk1 += exp(calc_risk(r, w1; X = returns) * scale)
+        risk2 += exp(calc_risk(r, w2; X = returns) * scale)
+        risk3 += exp(calc_risk(r, w3; X = returns) * scale)
+        unset_set_rm_properties!(r, solver_flag, sigma_flag, skew_flag, sskew_flag)
+    end
+    risk1 = log(risk1) / gamma
+    risk2 = log(risk2) / gamma
+    risk3 = log(risk3) / gamma
+    return risk1, risk2, risk3
+end
 function noc_risk_ret(port::Portfolio, type)
     (; bins, w_min, w_max, w_min_ini, w_max_ini, w_opt, rm, obj, kelly, class, w_ini, custom_constr, custom_obj, ohf, scalarisation) = type
 
@@ -50,7 +71,8 @@ function noc_risk_ret(port::Portfolio, type)
     w1 = if isempty(w_min)
         _w_min = optimise!(port,
                            Trad(; rm = rm, obj = MinRisk(), kelly = kelly, class = class,
-                                w_ini = w_min_ini, custom_constr = custom_constr, custom_obj = custom_obj))
+                                w_ini = w_min_ini, custom_constr = custom_constr, custom_obj = custom_obj,
+                                scalarisation = scalarisation))
         !isempty(_w_min) ? _w_min.weights : Vector{eltype(returns)}(undef, 0)
     else
         w_min
@@ -59,7 +81,8 @@ function noc_risk_ret(port::Portfolio, type)
     w2 = if isempty(w_max)
         _w_max = optimise!(port,
                            Trad(; rm = rm, obj = MaxRet(), kelly = kelly, class = class,
-                                w_ini = w_max_ini, custom_constr = custom_constr, custom_obj = custom_obj))
+                                w_ini = w_max_ini, custom_constr = custom_constr, custom_obj = custom_obj,
+                                scalarisation = scalarisation))
         !isempty(_w_max) ? _w_max.weights : Vector{eltype(returns)}(undef, 0)
     else
         w_max
@@ -85,7 +108,7 @@ function noc_risk_ret(port::Portfolio, type)
         ret3 = sum(log.(one(eltype(mu)) .+ returns * w3)) / size(returns, 1)
     end
 
-    risk1, risk2, risk3 = _noc_risks(rm, port, returns, sigma, w1, w2, w3)
+    risk1, risk2, risk3 = _noc_risks(scalarisation, rm, port, returns, sigma, w1, w2, w3)
 
     d_ret = (ret2 - ret1) / bins
     d_risk = (risk2 - risk1) / bins
