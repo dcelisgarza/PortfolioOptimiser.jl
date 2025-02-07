136cbdc486e359315fee64d1871615ffe3a78432
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2025-02-07 21:09:48 +0000

    See if this test doesn't hang.

--- test/ConstrainedOptimisationTests.jl
+++ test/ConstrainedOptimisationTests.jl
@@ -18,14 +18,17 @@ l = 2.0
                                                                                                                                  "max_step_fraction" => 0.75))))
     asset_statistics!(portfolio)
 
-    portfolio.long_t = 0.13
+    portfolio.long_t = 0.23
     w1 = optimise!(portfolio, Trad())
     idx = w1.weights .>= portfolio.long_t - sqrt(eps())
     @test isapprox(sum(w1.weights[idx]), portfolio.budget)
+    println("MinRisk 1")
 
+    portfolio.long_t = 0.47
     w2 = optimise!(portfolio, Trad(; obj = Sharpe(; rf = rf)))
     idx = w2.weights .>= portfolio.long_t - sqrt(eps())
     @test isapprox(sum(w2.weights[idx]), portfolio.budget)
+    println("Sharpe 1")
 
     portfolio.short = true
     portfolio.long_t = 0.31
@@ -41,11 +44,12 @@ l = 2.0
     @test isapprox(sum(w3.weights[nidx][nidx2]), portfolio.short_budget, rtol = 1e-6)
     @test isapprox(sum(w3.weights[pidx][pidx2]), portfolio.budget - portfolio.short_budget,
                    rtol = 1e-6)
+    println("MinRisk 2")
 
-    portfolio.long_t = 0.27
+    portfolio.long_t = 0.37
     portfolio.short_budget = -1
     portfolio.short_lb = -1
-    portfolio.short_t = -0.37
+    portfolio.short_t = -0.41
     w4 = optimise!(portfolio, Trad(; obj = Sharpe(; rf = rf), str_names = true))
     nidx = w4.weights .< 0
     pidx = .!nidx
@@ -54,6 +58,7 @@ l = 2.0
     @test isapprox(sum(w4.weights[nidx][nidx2]), portfolio.short_budget, rtol = 5e-8)
     @test isapprox(sum(w4.weights[pidx][pidx2]), portfolio.budget - portfolio.short_budget,
                    rtol = 5e-9)
+    println("Sharpe 2")
 end
 
 @testset "Management fees" begin
