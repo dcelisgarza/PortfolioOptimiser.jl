2b1f4203ba8a8cdb81ea5a47a66eb91a7b9a11b5
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2025-03-27 22:49:09 +0000

    Overload for VaRRG.

--- src/Utils/Utils.jl
+++ src/Utils/Utils.jl
@@ -8,14 +8,14 @@ for (op, name) ∈
          EDaR_r, RLDaR_r, Kurt, SKurt, GMD, RG, CVaRRG, TG, TGRG, OWA, BDVariance, NQSkew,
          NQSSkew, NSkew, NSSkew, Equal, WCVariance, TCM, TLPM, FTCM, FTLPM, Skewness,
          SSkewness, Kurtosis, SKurtosis, SchurParams, DRCVaR, NCOArgs, TrackingRM,
-         TurnoverRM, PortOptSolver),
+         TurnoverRM, PortOptSolver, VaRRG),
         ("SD", "Variance", "MAD", "SSD", "SVariance", "FLPM", "WR", "VaR", "CVaR", "EVaR",
          "EVaRRG", "RLVaR", "RLVaRRG", "DaR", "MDD", "ADD", "CDaR", "UCI", "EDaR", "RLDaR",
          "DaR_r", "MDD_r", "ADD_r", "CDaR_r", "UCI_r", "EDaR_r", "RLDaR_r", "Kurt", "SKurt",
          "GMD", "RG", "CVaRRG", "TG", "TGRG", "OWA", "BDVariance", "NQSkew", "NQSSkew",
          "NSkew", "NSSkew", "Equal", "WCVariance", "TCM", "TLPM", "FTCM", "FTLPM",
          "Skewness", "SSkewness", "Kurtosis", "SKurtosis", "SchurParams", "DRCVaR",
-         "NCOArgs", "TrackingRM", "TurnoverRM", "PortOptSolver"))
+         "NCOArgs", "TrackingRM", "TurnoverRM", "PortOptSolver", "VaRRG"))
     eval(quote
              Base.iterate(S::$op, state = 1) = state > 1 ? nothing : (S, state + 1)
              function Base.String(s::$op)
